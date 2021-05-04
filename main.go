d56b52b162962edf0b9151ec0fd0b3f68e9d241a
paul <423357+toothbrush@users.noreply.github.com>
2021-05-04 13:40:55 +1000

    main.go: Avoid fmt; 33% smaller binary.

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 	"time"
 )
 
+func printErr(err string) {
+	os.Stderr.Write([]byte(err))
+}
+
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Please provide a number of seconds to sleep.")
+		printErr("Please provide a number of seconds to sleep.\n")
 		os.Exit(1)
 	}
 
 	var delay = os.Args[1]
 	i, err := strconv.Atoi(delay)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Please give an integer-valued sleep delay.")
+		printErr("Please give an integer-valued sleep delay.\n")
 		os.Exit(1)
 	}
 
